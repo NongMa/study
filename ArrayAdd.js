8f4a7610766a07f1cd7e42ed5226f6676bdc81ec
NongMa <1005540091@qq.com>
2018-01-18 17:23:44 +0800

    ArrayAdd.js

---
+++ ArrayAdd.js
@@ -0,0 +1,10 @@
+Array.prototype.random=function(){
+    var that=this;
+    for(var i=0;i<that.length;i++){
+	    var newIndex=Math.floor(Math.random()*that.length);
+	    var itemIndex=that[newIndex]
+	    that[newIndex]=that[i];
+	    that[i]=itemIndex;
+    }
+    return that;
+}
